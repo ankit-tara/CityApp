29631578d07b41ca2418280bd85ce63be160f492
rajneet <rajneet@rwit.io>
2019-08-13 14:06:33 +0530

    updates

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import App from './App';
 import {name as appName} from './app.json';
 import { Provider } from 'react-redux'
 import store from './src/redux/store'
-
+import Map from './src/components/Map'
 const CityApp = ()=>{
     return(
         <Provider store={store}>
--- src/screens/ListByTag.js
+++ src/screens/ListByTag.js
@@ -17,13 +17,18 @@ import { getPostBytag } from "../Utils/Api.js";
 import { text_truncate, strip_html_tags } from "../Utils/Helpers.js";
 import { M_BOLD } from "../theme/fonts.js";
 import { APP_ORANGE } from "../theme/colors.js";
+var days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+
+const date = new Date()
+const today_day = date.getDay()
+
 const ListByTag = props => {
   const per_page = 10;
   const [tag, settag] = useState();
   const [posts, setposts] = useState([]);
   const [currentpage, setcurrentpage] = useState(1);
   const [loadMore, setloadMore] = useState(false);
-  const scrollY  = new Animated.Value(0)
+  const scrollY = new Animated.Value(0);
 
   useEffect(() => {
     if (props.navigation.state.params.item) {
@@ -37,6 +42,14 @@ const ListByTag = props => {
     }
   }, [props]);
 
+  getTime=(data)=>{
+    let day = days[today_day]
+    console.log(day)
+    console.log(data[day])
+
+    
+  }
+
   loadMoreData = () => {
     setloadMore(true);
     getPostBytag(tag.id, per_page, currentpage + 1)
@@ -49,19 +62,20 @@ const ListByTag = props => {
       .catch(e => console.log(e));
   };
 
-  isCloseToBottom = ({layoutMeasurement, contentOffset, contentSize}) => {
-    const paddingToBottom = 20
-    return layoutMeasurement.height + contentOffset.y >=
+  isCloseToBottom = ({ layoutMeasurement, contentOffset, contentSize }) => {
+    const paddingToBottom = 20;
+    return (
+      layoutMeasurement.height + contentOffset.y >=
       contentSize.height - paddingToBottom
-  }
-
+    );
+  };
 
   renderFooter = () => {
-    if (!posts.length || posts.length<10) return null;
+    if (!posts.length || posts.length < 10) return null;
     return (
-     // Footer View with Load More button
+      // Footer View with Load More button
       <View style={styles.footer}>
-       {/* <TouchableOpacity
+        {/* <TouchableOpacity
           activeOpacity={0.9}
           onPress={() => loadMoreData()}
           //On Click of button calling loadMoreData function to load more data
@@ -70,32 +84,35 @@ const ListByTag = props => {
           <Text style={styles.btnText}>Load More</Text>
           {loadMore && <ActivityIndicator style={{ marginLeft: 8 }} color={APP_ORANGE} />}
         </TouchableOpacity> */}
-      {loadMore && <ActivityIndicator style={{ marginLeft: 8 }} color={APP_ORANGE} />}
+        {loadMore && (
+          <ActivityIndicator style={{ marginLeft: 8 }} color={APP_ORANGE} />
+        )}
       </View>
     );
   };
-  
-  gotoPost=(post)=>{
-    props.navigation.navigate('Single',{
-      post:post
-    })
-  }
 
+  gotoPost = post => {
+    props.navigation.navigate("Single", {
+      post: post
+    });
+  };
 
   return (
     <View style={{ flex: 1 }}>
-      <ScrollView style={{ marginBottom: 50 }}
-    scrollEventThrottle={16}
-    onScroll={Animated.event(
-      [{ nativeEvent: { contentOffset: { y: scrollY } } }],
-      {
-        listener: event => {
-          if (this.isCloseToBottom(event.nativeEvent)) {
-            this.loadMoreData()
+      <ScrollView
+        style={{ marginBottom: 50 }}
+        scrollEventThrottle={16}
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { y: scrollY } } }],
+          {
+            listener: event => {
+              if (this.isCloseToBottom(event.nativeEvent)) {
+                this.loadMoreData();
+              }
+            }
           }
-        }
-      }
-    )}     >
+        )}
+      >
         <View style={styles.wrapper}>
           {tag && (
             <View>
@@ -107,50 +124,44 @@ const ListByTag = props => {
                 keyExtractor={item => `post-${item.id}`}
                 data={posts}
                 renderItem={post => (
-                  <TouchableOpacity onPress={()=>gotoPost(post.item)}>
-                  <View style={styles.place}>
-                    <View style={styles.left}>
-                      {post.item.fimg_url ? (
-                        <Image
-                          style={{ width: 50, height: 50, borderRadius: 25 }}
-                          source={{
-                            uri: post.item.fimg_url
-                          }}
-                        />
-                      ) : (
-                        <View
-                          style={{ borderRadius: 25, backgroundColor: "red" }}
-                        >
-                          <Icon
-                            name="image"
-                            size={30}
-                            color="#fff"
-                            style={{
-                              width: 50,
-                              height: 50,
-                              borderRadius: 25,
-                              backgroundColor: "gray",
-                              textAlign: "center",
-                              paddingTop:10
+                  <TouchableOpacity onPress={() => gotoPost(post.item)}>
+                    <View style={styles.place}>
+                      <View style={styles.left}>
+                        {post.item.fimg_url ? (
+                          <Image
+                            style={{ width: 50, height: 50, borderRadius: 25 }}
+                            source={{
+                              uri: post.item.fimg_url
                             }}
                           />
-                        </View>
-                      )}
-                    </View>
-                    <View style={styles.right}>
-                      <Text style={styles.title}>
-                        {post.item.title.rendered}
-                      </Text>
-                      {post.item.content.rendered != '' && (
-                      <Text style={styles.description}>
-                        {text_truncate(
-                          strip_html_tags(post.item.content.rendered),
-                          75
+                        ) : (
+                          <View
+                            style={{ borderRadius: 25, backgroundColor: "red" }}
+                          >
+                            <Icon
+                              name="image"
+                              size={30}
+                              color="#fff"
+                              style={styles.image}
+                            />
+                          </View>
                         )}
-                      </Text>)}
-
+                      </View>
+                      <View style={styles.right}>
+                        <Text style={styles.title}>
+                          {post.item.title.rendered}
+                        </Text>
+                        {post.item.content.rendered != "" && (
+                          <Text style={styles.description}>
+                            {text_truncate(
+                              strip_html_tags(post.item.content.rendered),
+                              75
+                            )}
+                          </Text>
+                        )}
+                       { post.item.acf && <Text>{getTime(post.item.acf)}</Text>}
+                      </View>
                     </View>
-                  </View>
                   </TouchableOpacity>
                 )}
                 // ItemSeparatorComponent={() => <View style={styles.separator} />}
@@ -165,14 +176,14 @@ const ListByTag = props => {
   );
 };
 ListByTag.navigationOptions = {
-  title: 'Places',
+  title: "Places",
   headerTitleStyle: {
     textAlign: "center",
     alignSelf: "center",
     flex: 1,
-    fontFamily:M_BOLD,
+    fontFamily: M_BOLD
   },
-  headerRight:<View/>
+  headerRight: <View />
 };
 export default ListByTag;
 
@@ -222,8 +233,8 @@ const styles = StyleSheet.create({
     fontSize: 15,
     // fontWeight: "bold",
     color: "rgba(0,0,0,0.5)",
-    textTransform:"capitalize",
-    fontFamily:M_BOLD
+    textTransform: "capitalize",
+    fontFamily: M_BOLD
   },
   header: {
     marginBottom: 20,
@@ -242,5 +253,13 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     paddingVertical: 10,
     borderRadius: 5
+  },
+  image: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: "gray",
+    textAlign: "center",
+    paddingTop: 10
   }
 });
